87b59fc2fec3f88187365d1bf49a2c0b1cff4133
pchanhdara79 <phomany@uwm.edu>
2026-05-10 16:35:59 -0500

    update homepage in views

--- RecruiterSystem/urls.py
+++ RecruiterSystem/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('signup/', Signup.as_view(), name='signup'),
     path('logout/', Logout.as_view(), name='logout'),
 
+    path('home/', views.home, name='home'),
     path('profile/', views.user_profile, name='user_profile'),
     path('admin-profile/', views.admin_profile, name='admin_profile'),
     path('manage-users/', views.manage_users, name='manage_users'),
