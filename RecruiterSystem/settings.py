e7ecefc85e59b39f184a92e0bf8fb28345677044
kelvinmahmuti <kmahmuti@uwm.edu>
2026-05-11 18:21:41 -0500

    resolve pyc conflicts

--- RecruiterSystem/settings.py
+++ RecruiterSystem/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'group5',
         'USER': 'root',
-        'PASSWORD': 'ADb3545!2026!!',
+        'PASSWORD': '$$KashIsTheGOAT$$GjirokasterAlbaniaBaby1221',
         'HOST': 'localhost',
         'PORT': '3306', # Default MySQL port
     }
