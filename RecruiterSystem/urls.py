2e0549c4c55f5679834243ba6d275c5fe05e8bbb
pchanhdara79 <phomany@uwm.edu>
2026-05-12 09:36:25 -0500

    Added event_detail for navigation to event details

--- RecruiterSystem/urls.py
+++ RecruiterSystem/urls.py
@@ -43,5 +43,5 @@ urlpatterns = [
     path('events/add/', views.add_event, name='add_event'),
     path('events/edit/<int:event_id>/', views.edit_event, name='edit_event'),
     path('events/delete/<int:event_id>/', views.delete_event, name='delete_event'),
-
+    path('events/<int:event_id>/', views.event_detail, name='event_detail'),
 ]
